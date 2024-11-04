c6eab9f26c8ba57f94e0bd2e320c71ce6a0f10be
yeabsera <yeab.dev@gmail.com>
2024-11-04 15:51:52 +0300

    add articles' parameters model

---
+++ lib/domain/models/articles_params.dart
@@ -0,0 +1,14 @@
+class ArticlesParams {
+  ArticlesParams({required this.period});
+  late final int period;
+
+  ArticlesParams.fromJson(Map<String, dynamic> json) {
+    period = json['period'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data["period"] = period;
+    return data;
+  }
+}
